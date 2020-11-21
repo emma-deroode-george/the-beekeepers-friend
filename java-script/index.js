239304c7720af2935e2ad335dcfc8e648bc98b11
emma-deroode-george <65256448+emma-deroode-george@users.noreply.github.com>
2020-11-20 20:22:26 -0600

    small changes

--- java-script/index.js
+++ java-script/index.js
@@ -1,11 +1,12 @@
-var modal = document.querySelector(".modal");
-var trigger = document.querySelector(".trigger");
-var closeButton = document.querySelector(".close-button");
-function toggleModal() {
-    modal.classList.toggle("show-modal");
-}
-function windowOnClick(event) {
-    if (event.target === modal) {
-        toggleModal();
+var current_h = null;
+var current_w = null;
+$('.pic1').hover(
+    function(){
+        current_h = $(this, 'img')[0].height;
+        current_w = $(this, 'img')[0].width;
+        $(this).animate({width: (current_w * 3), height: (current_h * 3)}, 300);
+    },
+    function(){
+        $(this).animate({width: current_w + 'px', height: current_h + 'px'}, 300);
     }
-}
+);
